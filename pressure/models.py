fa31ab46f62c234ffab34b1994cb2d98017d2e8b
1inchman <a.butenko.o@gmail.com>
2016-08-25 18:49:09 +0300

    Migrated to db

--- pressure/models.py
+++ pressure/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class User(models.Model):
     user_name = models.CharField(max_length=20)
-    password_hash = models.CharField(max_length=None)
+    password_hash = models.CharField(max_length=100)
 
     def __str__(self):
         return self.user_name
