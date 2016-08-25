296d51e0f1b2eb8211a0328880f7bcef291842c3
1inchman <a.butenko.o@gmail.com>
2016-08-25 18:41:40 +0300

    Added db models

--- pressure/models.py
+++ pressure/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_name = models.CharField(max_length=20)
+    password_hash = models.CharField(max_length=None)
+
+    def __str__(self):
+        return self.user_name
+
+
+class Measurment(models.Model):
+    systolic = models.FloatField()
+    diastolic = models.FloatField()
+    date = models.DateTimeField()
+    user_id = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.systolic
